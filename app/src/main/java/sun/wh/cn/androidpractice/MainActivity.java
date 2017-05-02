701df6e4f7dafa5ff2d60bba2bd03618b2e0155c
Marc SUN <710641245@qq.com>
2017-05-02 19:08:46 +0800

    intent partially

--- app/src/main/java/sun/wh/cn/androidpractice/MainActivity.java
+++ app/src/main/java/sun/wh/cn/androidpractice/MainActivity.java
@@ -3,6 +3,7 @@ package sun.wh.cn.androidpractice;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -64,6 +66,30 @@ public class MainActivity extends AppCompatActivity {
 
         //commit是同步方法，apply是异步方法
         edit.commit();
+
+        //dial
+        Button intent_dial = (Button) findViewById(R.id.intent_dial);
+        intent_dial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri tel = Uri.parse("tel:111111111");
+                Intent callIntent = new Intent(Intent.ACTION_DIAL, tel);
+                startActivity(callIntent);
+            }
+        });
+
+        //location
+        Button intent_location = (Button) findViewById(R.id.intent_location);
+        intent_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri location = Uri.parse("geo:0,0?q=1600+Amphitheatre+Parkway,+Mountain+View,+California");
+                // Or map point based on latitude/longitude
+                // Uri location = Uri.parse("geo:37.422219,-122.08364?z=14"); // z param is zoom level
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+                startActivity(mapIntent);
+            }
+        });
     }
 
     @Override
@@ -75,12 +101,26 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return false;
+        int itemId = item.getItemId();
+        switch (itemId) {
+            case R.id.action_settings:
+                openSettings();
+                return true;
+
+            case R.id.action_search:
+                openSearch();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void openSettings() {
+        Toast.makeText(this, "opening settings", Toast.LENGTH_LONG).show();
     }
 
     private void openSearch() {
+        Toast.makeText(this, "opening search", Toast.LENGTH_LONG).show();
     }
 }
